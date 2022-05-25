7b5241e3f694823478cad8679d5333e52ea994b4
anhbuicsa <anh.bui.csa@gmail.com>
2022-05-25 14:34:46 +0700

    change readme

--- gcp-tf-template/providers.tf
+++ gcp-tf-template/providers.tf
@@ -1,6 +1,7 @@
 provider "google" {
   project = var.project_id
   alias   = "tokengen"
+  # With this method, you also have the option of using more than one service account by specifying additional provider blocks with unique aliases.
   scopes = [
     "https://www.googleapis.com/auth/cloud-platform",
     "https://www.googleapis.com/auth/userinfo.email",
@@ -11,7 +12,8 @@ provider "google" {
 #tf refresh -target data.google_service_account_access_token.sa
 data "google_service_account_access_token" "sa" {
   provider               = google.tokengen
-  target_service_account = "<gcp_manage_state_service_account>"
+  target_service_account = "<gcp_manage_state_service_account>" 
+# With this method, you also get the token from another step
   lifetime               = "3500s"
   scopes = [
     "https://www.googleapis.com/auth/cloud-platform",
